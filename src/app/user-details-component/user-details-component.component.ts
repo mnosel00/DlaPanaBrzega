f240a246128dd76f06c225bf8a384417293231ca
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-06-09 00:20:37 +0200

    user details

--- src/app/user-details-component/user-details-component.component.ts
+++ src/app/user-details-component/user-details-component.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../interfaces/user.interface';
+import { ActivatedRoute } from '@angular/router';
+import { Task } from '../interfaces/task.interface';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-user-details-component',
   templateUrl: './user-details-component.component.html',
   styleUrls: ['./user-details-component.component.css']
 })
-export class UserDetailsComponentComponent {
+export class UserDetailsComponentComponent implements OnInit {
+  displayedColumns: string[] = ['ID', 'login', 'name', 'surname', 'role','assignedTasks'];
+  users: User[] = []
+  userOwner: User[] =[]
+  userLogin!: string
+  taskOptions : Task[] = []
+  taskAssigned : Task[] =[]
+  taskDone : Task[] = []
 
+  constructor(
+    private userService:UserService,
+    private route: ActivatedRoute,
+    private taskService : TaskService
+    ){
+    this.userService.getUsers().subscribe((user:User[])=>{
+      this.users = user
+    })
+    this.taskService.getTasks().subscribe((tasks: Task[])=>{
+      this.taskOptions = tasks
+    })
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params=>{
+      const login = params.get('id')
+      if(login!== null)
+      {
+        this.userLogin = login
+      }else
+      {
+        this.userLogin =''
+      }
+    })
+
+    const userToDisplayDetais = this.users.find((user:User)=> user.login ===this.userLogin)
+
+    
+    if(userToDisplayDetais!==undefined)
+    {
+      this.userOwner.push(userToDisplayDetais)
+    }else
+    {
+      console.log("error z detail user")
+    }
+
+    console.log(this.userOwner)
+    this.getTaskAssigned()
+    this.getTaskDone()
+   
+  }
+
+  getTaskAssigned()
+  {
+    this.taskAssigned = this.taskOptions.filter(task=> task.assignedUser?.login === this.userLogin && task.state !== 'Done')
+  }
+
+  getTaskDone()
+  {
+    this.taskDone = this.taskOptions.filter(task=> task.assignedUser?.login === this.userLogin && task.state === 'Done')
+  }
 }
